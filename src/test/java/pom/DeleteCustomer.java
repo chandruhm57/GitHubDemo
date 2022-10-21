31bbe1af846389bab3032c0116b1e654d6232033
chandra <chandruhm57@gmail.com>
2022-10-21 11:20:30 +0530

    modified loginpage and deletecustomerpage in POM

--- src/test/java/pom/DeleteCustomer.java
+++ src/test/java/pom/DeleteCustomer.java
@@ -1,5 +1,22 @@
 package pom;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class DeleteCustomer {
+	
+	WebDriver ldriver;
+	
+	public DeleteCustomer(WebDriver rdriver) {
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+	
+	By txtDeleteCustomer=By.xpath("");
+	
+	public void clickDeleteCustomer() {
+		ldriver.findElement(txtDeleteCustomer).click();
+	}
 
 }
--- src/test/java/pom/LoginPage.java
+++ src/test/java/pom/LoginPage.java
@@ -1,6 +1,9 @@
 package pom;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -12,5 +15,28 @@ public class LoginPage {
 		
 		PageFactory.initElements(rdriver, this);
 	}
+	
+	@FindBy(xpath="")
+	@CacheLookup
+	WebElement txtUsername;
+	
+	@FindBy(xpath="")
+	@CacheLookup
+	WebElement txtPassword;
+	
+	@FindBy(css="")
+	@CacheLookup
+	WebElement clkButton;
+	
+	public void setUsername(String username) {
+		txtUsername.sendKeys(username);
+	}
 
+	public void setPassword(String password) {
+		txtPassword.sendKeys(password);
+	}
+	
+	public void clickLoginButton() {
+		clkButton.click();
+	}
 }
