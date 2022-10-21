bcb1617cb21576e1cf30e65a3f5b842645e17be1
chandra <chandruhm57@gmail.com>
2022-10-21 09:05:27 +0530

    added constructor to LoginPage POM file

--- src/test/java/pom/AddCustomer.java
+++ src/test/java/pom/AddCustomer.java
@@ -1,5 +1,5 @@
 package pom;
 
 public class AddCustomer {
-
+	
 }
--- src/test/java/pom/LoginPage.java
+++ src/test/java/pom/LoginPage.java
@@ -1,5 +1,16 @@
 package pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+	
+	WebDriver ldriver;
+	
+	public LoginPage(WebDriver rdriver) {
+		ldriver=rdriver;
+		
+		PageFactory.initElements(rdriver, this);
+	}
 
 }
